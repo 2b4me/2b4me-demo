8a8c969f712ae23ddd21a0a7b0754e7fd766d7d7
Daniel Silva <daniel@silvanolte.com>
2012-06-25 21:13:03 -0400

    Added new email.css file for email styles; added new small version of 2b4me logo w/ blue background; more edits to promo to add contest winners; updates to the email templated, changed from css positioning to table layout for mass e-mail support; updated sendmail script to work properly by readining in input.

--- tools/contest/sendmail.groovy
+++ tools/contest/sendmail.groovy
@@ -2,37 +2,60 @@ import javax.mail.internet.*
 import javax.mail.*
 
 def host = 'mail.2b4me.com'
-def method = 'smtps'
-def port = '465'
+def method = 'smtp'
+def port = '25'
 def user = 'info@2b4me.com'
+def userName = '2b4me Launch Promotion'
 def passwd = 'Giorgio!@!Daniel'
 
 def props = new Properties()
 props.put('mail.smtp.host', host)
 props.put('mail.smtp.user', user)
 props.put('mail.smtp.port', port)
-props.put('mail.smtp.starttls.enable', 'true')
 props.put('mail.smtp.auth', 'true')
 
-def session = Session.getInstance(props, null)
-def msg = new MimeMessage(session)
+new File('input/contestants-for-email.txt').withReader { reader ->
+    
+    def x
+    while ((x = reader.readLine()) != null) {
+        def tokens = x.tokenize('\t')
+        def to = tokens[0]
+        def cnum = tokens[1]
+        def date = tokens[2]
+        def time = tokens[3]
+        def subject = 'Your 2b4me Contest Number'
+        def buffer = new StringBuffer()
 
-def to = 'daniel@silvanolte.com'
-def subject = 'test'
-def body = 'hello, world'
+        new File('email-template.html').withReader { reader2 ->
+            def y
+            while ((y = reader2.readLine()) != null) {
+                y = y.replace('#date', "${date} at ${time}")
+                y = y.replace('#cnum', cnum)
+                buffer.append(y)
+                buffer.append('\n')
+            }
+        }
 
-msg.setText(body)
-msg.setSubject(subject)
-msg.setFrom(new InternetAddress(user))
-msg.addRecipient(MimeMessage.RecipientType.TO, new InternetAddress(to))
+        def body = buffer.toString()
+        def session = Session.getInstance(props, null)
+        def msg = new MimeMessage(session)
+        msg.setContent(body, 'text/html')
+        msg.setSubject(subject)
+        msg.setFrom(new InternetAddress(user, userName))
+        msg.addRecipient(MimeMessage.RecipientType.TO, new InternetAddress(to))
 
-def transport = session.getTransport(method)
+        def transport = session.getTransport(method)
 
-println ("Connecting to ${host}:${port} over ${method} as ${user}")
-transport.connect(host, port.toInteger(), user, passwd)
+        println ("\nConnecting to ${host}:${port} over ${method} as ${user}")
+        transport.connect(host, port.toInteger(), user, passwd)
 
-println ('Sending message')
-transport.sendMessage(msg, msg.getAllRecipients())
+        println ('Sending message')
+        transport.sendMessage(msg, msg.getAllRecipients())
 
-println ("Closing connection to ${host}:${port}")
-transport.close()
+        println ("Closing connection to ${host}:${port}")
+        transport.close()
+        
+        this.sleep(5000)
+    }
+    
+}
