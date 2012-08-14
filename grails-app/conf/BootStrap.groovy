2e3b662a71db27abaf5e624fbe5820ed0481e59a
Daniel <daniel@silvanolte.com>
2012-08-14 13:13:39 -0400

    Updated the email messages to include an opt out link, and refreshed all the templates both in the contest directory and in BootStrap for the application database.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -114,14 +114,19 @@ class BootStrap {
 
 <p>Thank you for signing up at 2b4me.com. We are very excited to launch this company and
    to connect people with some of the really cool stuff South Florida has to offer.</p>
+
 <p>In our ramp up to launch we are running a promotion to help spread the word on our
    company AND give you a chance to win some major cash and prizes in the process.</p>
+
 <p>You signed up on #var and your contest number is:</p>
+
 <h1>#var</h1>
+
 <p>Keep this number safe. Whenever we run a contest drawing, we will publish the winners
    on the website <a href="http://2b4me.com">http://2b4me.com</a>, so check back often.
    We will be notifying on our <a href="http://facebook.com/2b4me">Facebook</a> page when
    new winners are published.</p>
+
 <p>Thank you again for your participation. And remember to please tell your friends!</p>
 
 <p>&nbsp;</p>
@@ -132,10 +137,17 @@ class BootStrap {
 <p>Like us on Facebook: <a href="http://facebook.com/2b4me">facebook.com/2b4me</a><br/>
    Follow us on Twitter: <a href="http://twitter.com/2b4me_deals">@2b4me_deals</a></p>
 
-<!--
-Don't care for these emails?
-You can personalize your notification settings, or click here to unsubscribe from all emails with no fuss.
- -->
+<p>&nbsp;</p>
+
+<hr/>
+
+<p>Don't care for these emails? You can
+   <a href="mailto:info@2b4me.com?subject=UNSUBSCRIBE">click here</a> to unsubscribe from
+   all future contest notifications with no fuss:
+   <a href="mailto:info@2b4me.com?subject=UNSUBSCRIBE">Unsubscribe</a>.</p>
+
+<p><strong>2b4me.com Inc.</strong>, 55 Merrick Way, Coral Gables, FL 33134. To contact us,
+   <a href="mailto:info@2b4me.com">click here</a>.</p>
 
 </body>
 
@@ -146,18 +158,28 @@ You can personalize your notification settings, or click here to unsubscribe fro
 
 <body> <!--  style="font-family: arial,sans-serif; font-size: 12px" -->
 
-<p>We have published the winners that were selected in today's drawing on the
-   website! Go to <a href="http://2b4me.com">http://2b4me.com</a> to see if you've
-   won!</p>
+<p>We have recently published the latest winners in our Launch Promotion contest on the
+   website!</p>
 
-<p>We've given away several cash prizes thus far, and we're going to do it
-   again on #var! You've already signed up so there's nothing more for you to do.
-   Just check back to see if your number was drawn.</p>
+<p>As a reminder, your contest number is:</p>
+
+<h1>#var</h1>
+
+<p>All you have to do is go to <a href="http://2b4me.com">http://2b4me.com</a> and see
+   if you're number was drawn! <em>If you won, send an email to info@2b4me.com with your
+   legal name and address so that we can mail you your prize.</em></p>
+
+<p>We've given away several $50 and $20 cash prizes thus far, and we're going to do it
+   again soon! You've already signed up so there's nothing more for you to do.
+   Just keeping checking the site to see if your number is drawn in a future drawing.</p>
 
 <p>If you have any questions, please send them to
-   <a href="mailto:info@2b4me.com">info@2b4me.com</a> and we'll be more than happy to assist.</p>
+   <a href="mailto:info@2b4me.com">info@2b4me.com</a> and we'll be more than happy to
+   assist.</p>
 
 <p>Thank you again for your participation. And remember to please tell your friends!</p>
+
+<p>Good luck!</p>
 
 <p>&nbsp;</p>
 
@@ -167,10 +189,17 @@ You can personalize your notification settings, or click here to unsubscribe fro
 <p>Like us on Facebook: <a href="http://facebook.com/2b4me">facebook.com/2b4me</a><br/>
    Follow us on Twitter: <a href="http://twitter.com/2b4me_deals">@2b4me_deals</a></p>
 
-<!--
-Don't care for these emails?
-You can personalize your notification settings, or click here to unsubscribe from all emails with no fuss.
- -->
+<p>&nbsp;</p>
+
+<hr/>
+
+<p>Don't care for these emails? You can
+   <a href="mailto:info@2b4me.com?subject=UNSUBSCRIBE">click here</a> to unsubscribe from
+   all future contest notifications with no fuss:
+   <a href="mailto:info@2b4me.com?subject=UNSUBSCRIBE">Unsubscribe</a>.</p>
+
+<p><strong>2b4me.com Inc.</strong>, 55 Merrick Way, Coral Gables, FL 33134. To contact us,
+   <a href="mailto:info@2b4me.com">click here</a>.</p>
 
 </body>
 
@@ -183,9 +212,14 @@ You can personalize your notification settings, or click here to unsubscribe fro
 
 <p>Hi #var,</p>
 
-<p>Congratulations on winning the #var prize from the #var drawing! We're excited to spread the word around on 2b4me and what we want to offer the South Florida market (and beyond)! Please allow up to two weeks for your prize to arrive. And because this is an on-going promotion, you are eligible to win at every drawing until the end of the contest! So keep checking back to see if your contest number is drawn in a future drawing.</p>
+<p>Congratulations on winning the #var from the #var drawing! We're excited to spread the
+   word around on 2b4me and what we want to offer the South Florida market (and beyond)!
+   Please allow up to two weeks for your prize to arrive. And because this is an on-going
+   promotion, you are eligible to win at every drawing until the end of the contest! So
+   keep checking back to see if your contest number is drawn in a future drawing.</p>
 
-<p>Thank you for participating, good luck in future drawings and please remember to tell your friends!</p>
+<p>Thank you for participating, good luck in future drawings and please remember to tell
+   your friends!</p>
 
 <p>&nbsp;</p>
 
@@ -195,10 +229,17 @@ You can personalize your notification settings, or click here to unsubscribe fro
 <p>Like us on Facebook: <a href="http://facebook.com/2b4me">facebook.com/2b4me</a><br/>
    Follow us on Twitter: <a href="http://twitter.com/2b4me_deals">@2b4me_deals</a></p>
 
-<!--
-Don't care for these emails?
-You can personalize your notification settings, or click here to unsubscribe from all emails with no fuss.
- -->
+<p>&nbsp;</p>
+
+<hr/>
+
+<p>Don't care for these emails? You can
+   <a href="mailto:info@2b4me.com?subject=UNSUBSCRIBE">click here</a> to unsubscribe from
+   all future contest notifications with no fuss:
+   <a href="mailto:info@2b4me.com?subject=UNSUBSCRIBE">Unsubscribe</a>.</p>
+
+<p><strong>2b4me.com Inc.</strong>, 55 Merrick Way, Coral Gables, FL 33134. To contact us,
+   <a href="mailto:info@2b4me.com">click here</a>.</p>
 
 </body>
 
@@ -221,10 +262,17 @@ You can personalize your notification settings, or click here to unsubscribe fro
 <p>Like us on Facebook: <a href="http://facebook.com/2b4me">facebook.com/2b4me</a><br/>
    Follow us on Twitter: <a href="http://twitter.com/2b4me_deals">@2b4me_deals</a></p>
 
-<!--
-Don't care for these emails?
-You can personalize your notification settings, or click here to unsubscribe from all emails with no fuss.
- -->
+<p>&nbsp;</p>
+
+<hr/>
+
+<p>Don't care for these emails? You can
+   <a href="mailto:info@2b4me.com?subject=UNSUBSCRIBE">click here</a> to unsubscribe from
+   all future contest notifications with no fuss:
+   <a href="mailto:info@2b4me.com?subject=UNSUBSCRIBE">Unsubscribe</a>.</p>
+
+<p><strong>2b4me.com Inc.</strong>, 55 Merrick Way, Coral Gables, FL 33134. To contact us,
+   <a href="mailto:info@2b4me.com">click here</a>.</p>
 
 </body>
 
