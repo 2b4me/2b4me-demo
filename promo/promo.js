4bd72a7030858144e8d36587a20d173624be135e
Daniel <daniel@silvanolte.com>
2012-08-28 09:38:29 -0400

    Updates related to 8/28 drawing.

--- promo/promo.js
+++ promo/promo.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-    countdown(2012, 8, 31); // kick off the countdown
+    countdown(2012, 8, 31); // kick off the countdown, should be one day less than what is published
+                            // so that the counter counts down to midnight of the drawing date
     
     $('.nyi').click(function(event){
        alert('not yet implemented');
